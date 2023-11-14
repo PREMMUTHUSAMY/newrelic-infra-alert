6fbb93684b9769c04b9dc540b331582b35fab05d
root <root@newrelic-iac.jzfyxbj3ofjuvizrmiojtnxv1c.bx.internal.cloudapp.net>
2023-11-14 06:22:30 +0000

    Configure the Entity value to the var file

--- entity.tf
+++ entity.tf
@@ -1,5 +1,5 @@
 data "newrelic_entity" "infra_monitor" {
-  name = "Terraform" # Must be an exact match to your application name in New Relic
-  domain = "INFRA" # or BROWSER, INFRA, MOBILE, SYNTH, depending on your entity's domain
-  type = "HOST"
+  name = var.entity_name 
+  domain = var.entity_domain
+  type = var.entity_type
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,22 @@
 variable "api_key" {
-  default     = # Please your API_KEY
+  default     = # Your API_KEY
 }
 
 variable "account_id" {
-  default     = # Please your Account_Id
+  default     = # Your Account_Id
 }
 
 variable "region" {
-  default     = # Please Your region either U.S or EU
+  default     = # Your region either U.S or EU
+}
+variable "entity_name" {
+  default = # Must be an exact match to your application name in New Relic
 }
 
+variable "entity_domain" {
+  default = # BROWSER, INFRA, MOBILE, SYNTH, depending on your entity's domain
+}
+
+variable "entity_type" {
+  default = # HOST, APPLICATION, etc.,.
+}
