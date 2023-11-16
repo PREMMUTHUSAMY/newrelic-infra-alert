2e2ba27586d857c02ab1e3bd72e8a1787ac6a1be
Dhanush Mahadevan <dhanushlogesh62@gmail.com>
2023-11-16 15:32:37 +0000

    Updated notify_channel.tf as subject in property

--- notify_channel.tf
+++ notify_channel.tf
@@ -4,7 +4,7 @@ resource "newrelic_notification_channel" "team_email_channel" {
   destination_id = newrelic_notification_destination.team_email_destination.id
   product = "IINT"
 property {
-    key = "customDetailsEmail"
+    key = "subject"
     value = "[HOST-${data.newrelic_entity.infra_monitor.name}]-issue Title - {{issueTitle}}"
   }
 }
